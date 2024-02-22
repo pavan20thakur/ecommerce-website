6c329762a021bd95cfc20a478cabe04a7c2eaba4
Pavan Thakur <pt20112003@gmail.com>
2024-02-22 16:44:58 +0530

    Updating frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import PageNotFound from './pages/PageNotFound';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import Dashboard from './user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
+import AdminRoute from './components/Routes/AdminRoute';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
+import Users from './pages/Admin/Users';
+import Orders from './user/Orders';
+import Profile from './user/Profile';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<PrivateRoute />}>
+        <Route path='user' element={<Dashboard />} />
+        <Route path='user/orders' element={<Orders />} />
+        <Route path='user/profile' element={<Profile />} />
+      </Route>
+      <Route path='/dashboard' element={<AdminRoute />}>
+        <Route path='admin' element={<AdminDashboard />} />
+        <Route path='admin/create-category' element={<CreateCategory />} />
+        <Route path='admin/create-product' element={<CreateProduct />} />
+        <Route path='admin/users' element={<Users />} />
+      </Route>
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/policy' element={<Policy />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useAuth } from "../context/auth";
+import Layout from './../components/Layouts/Layout';
+
+const HomePage = () => {
+  const [auth, setAuth] = useAuth();
+  return (
+    <Layout title={"Best offers "}>
+      <h1>HomePage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    </Layout>
+  );
+};
+
+export default HomePage;
